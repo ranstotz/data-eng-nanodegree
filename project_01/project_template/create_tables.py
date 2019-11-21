5dc6ffd983312bf34d1ebf59f7e21ce8b434ae1e
ranstotz <ryananstotz@gmail.com>
2019-11-20 19:11:41 -0500

    completed the task of creating tables in create_tables.py.

--- project_01/project_template/create_tables.py
+++ project_01/project_template/create_tables.py
@@ -1,47 +1,80 @@
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+from sql_queries import create_table_queries, drop_table_queries, test_table
 
 
 def create_database():
     # connect to default database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    try:
+        conn = psycopg2.connect(
+            "host=127.0.0.1 dbname=studentdb user=student password=student")
+    except psycopg2.Error as e:
+        print("Error: Could not make connection to the Postgres database")
+        print(e)
+
     conn.set_session(autocommit=True)
-    cur = conn.cursor()
-    
+
+    try:
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Error: Could not get cursor to the Database")
+        print(e)
+
     # create sparkify database with UTF8 encoding
-    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
-    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    try:
+        cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    except psycopg2.Error as e:
+        print("Error dropping database")
+        print(e)
+    try:
+        cur.execute(
+            "CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+    except psycopg2.Error as e:
+        print("Error creating database")
 
     # close connection to default database
-    conn.close()    
-    
+    conn.close()
+
     # connect to sparkify database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    try:
+        conn = psycopg2.connect(
+            "host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    except psycopg2.Error as e:
+        print("Error creating database")
+        print(e)
+
     cur = conn.cursor()
-    
     return cur, conn
 
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+        try:
+            cur.execute(query)
+        except psycopg2.Error as e:
+            print("Error dropping table")
+            print(e)
 
 
 def create_tables(cur, conn):
     for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+        try:
+            cur.execute(query)
+        except psycopg2.Error as e:
+            print("Error creating table")
+            print(e)
 
 
 def main():
     cur, conn = create_database()
-    
+
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
     conn.close()
 
 
+#
+# MAIN FUNCTION
+#
 if __name__ == "__main__":
     main()
--- project_01/project_template/sql_queries.py
+++ project_01/project_template/sql_queries.py
@@ -1,52 +1,87 @@
+import psycopg2
+
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays \
+    (songplay_id int, start_time bigint, user_id varchar, level varchar, \
+        song_id varchar, artist_id varchar, session_id int, location varchar, \
+            user_agent varchar)
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users \
+    (user_id varchar, first_name varchar, last_name varchar, gender varchar, \
+        level varchar)
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs \
+    (song_id varchar, title varchar, artist_id varchar, year int, duration numeric)
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists \
+    (song_id varchar, name varchar, location varchar, latitude numeric, \
+        longitude numeric)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time \
+    (start_time bigint, hour int, day varchar, week int, month int, year int, \
+        weekday boolean )
 """)
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = ("""INSERT INTO songplays \
+    (songplay_id, start_time, user_id, level, song_id, artist_id, \
+        session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users \
+    (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs \
+    (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
 """)
 
-artist_table_insert = ("""
+artist_table_insert = ("""INSERT INTO artists \
+    (song_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
 """)
 
-
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time \
+    (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
+
+# Test song tables to ensure existence
+
+
+def print_table(cur, table_name):
+
+    query = "SELECT * FROM " + table_name + " LIMIT 5;"
+    try:
+        cur.execute(query)
+    except psycopg2.Error as e:
+        print("Error printing table w/ SELECT *")
+        print(e)
+    row = cur.fetchone()
+    while row:
+        print(row)
+        row = cur.fetchone()
+
 
 # FIND SONGS
-
-song_select = ("""
+song_select = ("""SELECT * FROM songs
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [songplay_table_create, user_table_create,
+                        song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop,
+                      song_table_drop, artist_table_drop, time_table_drop]
