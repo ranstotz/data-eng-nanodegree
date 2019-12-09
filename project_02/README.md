cf323515ad0b6daa3b558b8f817972b300846e54
ranstotz <ryananstotz@gmail.com>
2019-12-08 22:55:25 -0500

    finish project etl and readme description.

--- project_02/README.md
+++ project_02/README.md
@@ -1,6 +1,49 @@
 # Project 2: Data Modeling with Apache Cassandra
 
-## private notes for project
+Author: Ryan Anstotz
 
-- how to check cassandra status `nodetool status`
-- Restart service : `sudo systemctl restart cassandra`
+## Background
+
+A startup called Sparkify wants to analyze the data they've been collecting on songs and user activity on their new music streaming app. The analysis team is particularly interested in understanding what songs users are listening to. Currently, there is no easy way to query the data to generate the results, since the data reside in a directory of CSV files on user activity on the app.
+
+They'd like a data engineer to create an Apache Cassandra database which can create queries on song play data to answer the questions, and wish to bring you on the project. Your role is to create a database for this analysis. You'll be able to test your database by running queries given to you by the analytics team from Sparkify to create the results.
+
+## Summary
+
+### Project Setup
+
+To begin the project, data from the project workspace in the Jupyter notebook was compressed via tar and gzip and transferred it onto my local machine. Additionally, I installed Apache Cassandra and ran the local instance. The Python notebook was converted to a standard Python file (.py) for development.
+
+### Development
+
+This project required the creation of an ETL: pipeline to collect and consolidate data from multiple CSV files into a single CSV file for consumption. This file was saved as `event_datafile_new.cvs`. Once the data was pre-processed, three queries were provided. A table was designed per each query. The queries were as follows:
+
+    1.) Give me the artist, song title and song's length in the music app history that was heard during sessionId = 338, and itemInSession = 4.
+
+    2.) Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name) for userid = 10, sessionid = 182.
+
+    3.) Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'.
+
+Each of these queries prints the results to the console. Please reference comments in code for additional information and details regarding the code and design of the system.
+
+### Build Instructions
+
+A run script (run.sh) was created to execute the code. Use `bash run.sh` to get the results.
+
+### Results
+
+The results per each query was as follows:
+
+- Query 1:
+  Faithless Music Matters (Mark Knight Dub) 495.30731201171875
+
+- Query 2:
+  Down To The Bone Keep On Keepin' On Sylvie Cruz
+  Three Drives Greece 2000 Sylvie Cruz
+  Sebastien Tellier Kilometer Sylvie Cruz
+  Lonnie Gordon Catch You Baby (Steve Pitron & Max Sanna Radio Edit) Sylvie Cruz
+
+- Query 3:
+  Jacqueline Lynch
+  Tegan Levine
+  Sara Johnson
--- project_02/src/etl.py
+++ project_02/src/etl.py
@@ -4,22 +4,23 @@
 # Import Python packages
 import sys
 import os
-import pandas as pd
+from cassandra.cluster import Cluster
 import cassandra
+import pandas as pd
+import numpy as np
 import re
 import glob
-import numpy as np
 import json
 import csv
 
-# # Part I. ETL Pipeline for Pre-Processing the Files
-# Preprocess the data
+# ====================
+# Part I. ETL Pipeline for Pre-Processing the Files
+#
 
 # Get current folder and subfolder event data
 # Since the run.sh script is executed from the parent folder, consider
 # the getcwd() as the parent dir.
 filepath = os.getcwd() + '/data'
-print(filepath)
 
 # Create a for loop to create a list of files and collect each filepath
 for root, dirs, files in os.walk(filepath):
@@ -30,7 +31,6 @@ for root, dirs, files in os.walk(filepath):
 
 
 # Process the files to create the single data file csv that will be used for Apache Casssandra tables
-
 # initiating an empty list of rows that will be generated from each file
 full_data_rows_list = []
 
@@ -49,131 +49,225 @@ for f in file_path_list:
             # print(line)
             full_data_rows_list.append(line)
 
-# uncomment the code below if you would like to get total number of rows
-# print(len(full_data_rows_list))
-# uncomment the code below if you would like to check to see what the list of event data rows will look like
-# print(full_data_rows_list)
 
-# creating a smaller event data csv file called event_datafile_full csv that will be used to insert data into the \
-# Apache Cassandra tables
+# Creat a smaller event data csv file called event_datafile_full csv that will
+# be used to insert data into the Apache Cassandra tables
 csv.register_dialect('myDialect', quoting=csv.QUOTE_ALL, skipinitialspace=True)
 
 with open('event_datafile_new.csv', 'w', encoding='utf8', newline='') as f:
     writer = csv.writer(f, dialect='myDialect')
     writer.writerow(['artist', 'firstName', 'gender', 'itemInSession', 'lastName',
-                     'length',                'level', 'location', 'sessionId', 'song', 'userId'])
+                     'length', 'level', 'location', 'sessionId', 'song', 'userId'])
     for row in full_data_rows_list:
         if (row[0] == ''):
             continue
         writer.writerow((row[0], row[2], row[3], row[4], row[5],
                          row[6], row[7], row[8], row[12], row[13], row[16]))
 
+# ====================
+# Part II. Complete the Apache Cassandra coding portion of your project.
+#
 
-# check the number of rows in your csv file
-with open('event_datafile_new.csv', 'r', encoding='utf8') as f:
-    print(sum(1 for line in f))
+# Create connection to the database and a session
+try:
+    cluster = Cluster(['127.0.0.1'])
+    session = cluster.connect()
+except Exception as e:
+    print(e)
 
-'''
-# # Part II. Complete the Apache Cassandra coding portion of your project. 
-# 
-# ## Now you are ready to work with the CSV file titled <font color=red>event_datafile_new.csv</font>, located within the Workspace directory.  The event_datafile_new.csv contains the following columns: 
-# - artist 
-# - firstName of user
-# - gender of user
-# - item number in session
-# - last name of user
-# - length of the song
-# - level (paid or free song)
-# - location of the user
-# - sessionId
-# - song title
-# - userId
-# 
-# The image below is a screenshot of what the denormalized data should appear like in the <font color=red>**event_datafile_new.csv**</font> after the code above is run:<br>
-# 
-# <img src="images/image_event_datafile_new.jpg">
+# Create a keyspace named project_02
+try:
+    session.execute("""
+    CREATE KEYSPACE IF NOT EXISTS project_02
+    WITH REPLICATION =
+    { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }"""
+                    )
+except Exception as e:
+    print(e)
 
-# ## Begin writing your Apache Cassandra code in the cells below
+# Connect to keyspace
+try:
+    session.set_keyspace('project_02')
+except Exception as e:
+    print(e)
 
-# #### Creating a Cluster
+# ==========
+# Query 1:  Give me the artist, song title and song's length in the
+# music app history that was heard during sessionId = 338, and itemInSession = 4
+#
 
-# This should make a connection to a Cassandra instance your local machine 
-# (127.0.0.1)
+print("\nQuery 1 results: \n")
 
-from cassandra.cluster import Cluster
-cluster = Cluster()
-
-# To establish connection and begin executing queries, need a session
-session = cluster.connect()
-
-
-# #### Create Keyspace
-
-# TO-DO: Create a Keyspace 
+# Ensure table is dropped prior to creation.
+query = "DROP TABLE IF EXISTS music_library_0"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
 
-# #### Set Keyspace
+# Create music library for query 1
+query = "CREATE TABLE IF NOT EXISTS music_library_0 "
+query = query + "(session_id int, item_in_session int, artist_name text, \
+                song_title text, song_length float, PRIMARY KEY (session_id, \
+                item_in_session))"
 
+try:
+    session.execute(query)
+except Exception as e:
+    print(e)
 
-# TO-DO: Set KEYSPACE to the keyspace specified above
-
-
-# ### Now we need to create tables to run the following queries. Remember, with Apache Cassandra you model the database tables on the queries you want to run.
-
-# ## Create queries to ask the following three questions of the data
-# 
-# ### 1. Give me the artist, song title and song's length in the music app history that was heard during  sessionId = 338, and itemInSession  = 4
-# 
-# 
-# ### 2. Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name) for userid = 10, sessionid = 182
-#     
-# 
-# ### 3. Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
-# 
-# 
-# 
-
-
-## TO-DO: Query 1:  Give me the artist, song title and song's length in the music app history that was heard during \
-## sessionId = 338, and itemInSession = 4
-
-# We have provided part of the code to set up the CSV file. Please complete the Apache Cassandra code below#
+# We have provided part of the code to set up the CSV file. Please complete the
+# Apache Cassandra code below
 file = 'event_datafile_new.csv'
 
-with open(file, encoding = 'utf8') as f:
+with open(file, encoding='utf8') as f:
     csvreader = csv.reader(f)
-    next(csvreader) # skip header
+    next(csvreader)  # skip header
     for line in csvreader:
-## TO-DO: Assign the INSERT statements into the `query` variable
-        query = "<ENTER INSERT STATEMENT HERE>"
-        query = query + "<ASSIGN VALUES HERE>"
-        ## TO-DO: Assign which column element should be assigned for each column in the INSERT statement.
-        ## For e.g., to INSERT artist_name and user first_name, you would change the code below to `line[0], line[1]`
-        session.execute(query, (line[#], line[#]))
+        # Create INSERT statement
+        query = "INSERT INTO music_library_0 (session_id, item_in_session, artist_name, \
+            song_title, song_length)"
+        query = query + "VALUES (%s, %s, %s, %s, %s)"
+        # Assign columns for INSERT query.
+        session.execute(query, (int(line[8]), int(
+            line[3]), line[0], line[9], float(line[5])))
+
+# Verify the data was entered into the table by executing query
+query = "select artist_name, song_title, song_length from music_library_0 \
+    WHERE session_id=338 AND item_in_session=4"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+
+for row in rows:
+    print(row.artist_name, row.song_title, row.song_length)
+
+# ==========
+# Query 2: Give me only the following: name of artist, song (sorted by itemInSession)
+# and user (first and last name) for userid = 10, sessionid = 182
+#
+
+print("\nQuery 2 results: \n")
+
+# Ensure table is dropped prior to creation.
+query = "DROP TABLE IF EXISTS music_library_1"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
 
-# #### Do a SELECT to verify that the data have been inserted into each table
+# Create music library for query 2. Add item_in_session to primary key to sort by it
+query = "CREATE TABLE IF NOT EXISTS music_library_1 "
+query = query + "(session_id int, user_id int, item_in_session int, artist_name text, song_title text, \
+                user_first_name text, user_last_name text, PRIMARY KEY (session_id, user_id, \
+                    item_in_session))"
 
-## TO-DO: Add in the SELECT statement to verify the data was entered into the table
+try:
+    session.execute(query)
+except Exception as e:
+    print(e)
+
+# We have provided part of the code to set up the CSV file. Please complete the
+# Apache Cassandra code below
+file = 'event_datafile_new.csv'
+
+with open(file, encoding='utf8') as f:
+    csvreader = csv.reader(f)
+    next(csvreader)  # skip header
+    for line in csvreader:
+        # Create INSERT statement
+        query = "INSERT INTO music_library_1 (session_id, user_id, item_in_session, artist_name, song_title, \
+                user_first_name, user_last_name)"
+        query = query + "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        # Assign columns for INSERT query.
+        session.execute(query, (int(line[8]), int(line[10]), int(line[3]),
+                                line[0], line[9], line[1], line[4]))
+
+# Verify the data was entered into the table by executing query
+query = "select artist_name, song_title, user_first_name, user_last_name from music_library_1 \
+    WHERE session_id=182 AND user_id=10"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+
+for row in rows:
+    print(row.artist_name, row.song_title,
+          row.user_first_name, row.user_last_name)
+
+# ==========
+# Query 3: Give me every user name (first and last) in my music app history who
+# listened to the song 'All Hands Against His Own'
+#
+
+print("\nQuery 3 Results: \n")
+
+# Ensure table is dropped prior to creation.
+query = "DROP TABLE IF EXISTS music_library_2"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
 
-# ### COPY AND REPEAT THE ABOVE THREE CELLS FOR EACH OF THE THREE QUESTIONS
+# Create music library for query 3
+query = "CREATE TABLE IF NOT EXISTS music_library_2 "
+query = query + "(song_title text, user_id int, user_first_name text, \
+                    user_last_name text, PRIMARY KEY (song_title, user_id))"
+
+try:
+    session.execute(query)
+except Exception as e:
+    print(e)
+
+# Data file for consumption
+file = 'event_datafile_new.csv'
+
+with open(file, encoding='utf8') as f:
+    csvreader = csv.reader(f)
+    next(csvreader)  # skip header
+    for line in csvreader:
+        # Create INSERT statement
+        query = "INSERT INTO music_library_2 (song_title, user_id, \
+                user_first_name, user_last_name)"
+        query = query + "VALUES (%s, %s, %s, %s)"
+        # Assign columns for INSERT query.
+        session.execute(query, (line[9], int(line[10]), line[1], line[4]))
+
+# Verify the data was entered into the table by executing query
+query = "select user_first_name, user_last_name from music_library_2 \
+    WHERE song_title='All Hands Against His Own'"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+
+for row in rows:
+    print(row.user_first_name, row.user_last_name)
 
 
-## TO-DO: Query 2: Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name)\
-## for userid = 10, sessionid = 182
+# Drop the tables
+query = "DROP TABLE IF EXISTS music_library_0"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
+query = "DROP TABLE IF EXISTS music_library_1"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
-## TO-DO: Query 3: Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
+query = "DROP TABLE IF EXISTS music_library_2"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
 
-
-# ### Drop the tables before closing out the sessions
-
-## TO-DO: Drop the table before closing out the sessions
-
-# ### Close the session and cluster connection¶
-
+# Close the session and cluster connections
 session.shutdown()
 cluster.shutdown()
-
-'''
